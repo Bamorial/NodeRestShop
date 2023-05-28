57e768d19135a6f16d1c96bd38e64cc47df0a8fb
Bamorial <107000074+Bamorial@users.noreply.github.com>
2023-05-28 21:58:07 +0300

    almost mvp

--- api/models/orders.js
+++ api/models/orders.js
@@ -2,8 +2,8 @@ const mongoose  = require('mongoose');
 
 const orderSchema= mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
     quantity: {type: Number, default: 1},
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
     name: {type: String},
     address: {type:String, reqire:true},
     phone: {type:String, reqire:true},
